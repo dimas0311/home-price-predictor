40df99e4feb670a1fb4b47d86e3b50ea78ad85d2
bagus0300 <asamieto965@gmail.com>
2024-10-17 18:53:09 +0700

    fix : filter by baths

--- app/explore/main.jsx
+++ app/explore/main.jsx
@@ -12,41 +12,28 @@ import moment from "moment";
 const { RangePicker } = DatePicker;
 
 export const MainPage = () => {
-  const { data, loading } = useData();
+  const { homeData, stateData, loading } = useData();
 
-  const [filteredData, setFilteredData] = useState(data);
-  const [titleSearchTerm, setTitleSearchTerm] = useState("");
+  const [filteredData, setFilteredData] = useState(homeData);
+  const [bedsSearchTerm, setBedsSearchTerm] = useState("");
+  const [bathsSearchTerm, setBathsSearchTerm] = useState("");
   const [citySearchTerm, setCitySearchTerm] = useState("");
   const [dateRange, setDateRange] = useState(null);
   const [priceRange, setPriceRange] = useState([0, 100000000]);
   const [options, setOptions] = useState([]);
+  const [bOptions, setBOptions] = useState([]);
   const [displayCount, setDisplayCount] = useState(60);
 
-
+  const [selectedCityData, setSelectedCityData] = useState(null);
 
   useEffect(() => {
-    setFilteredData(data);
-  }, [data]);
-  console.log('en', data?.length)
-
-  const titleOptions = Array.from(
-    new Set(data.map((home) => home?.title))
-  ).map((title) => ({ value: title, label: title }));
-
-
-
-  const formatCityName = (cityName) => {
-    // Remove 'city' and spaces, then capitalize only the first letter
-    return cityName
-      .replace(/\s*city\s*/i, '')
-      .replace(/\s+/g, '')
-      .toLowerCase()
-      .replace(/^(.)/, match => match.toUpperCase());
-  };
+    setFilteredData(homeData);
+  }, [homeData]);
+  console.log("en", homeData?.length);
 
   const cityOptions = Array.from(
     new Set(
-      data
+      homeData
         .map((home) => home?.city)
         .filter((city) => city && city.trim() !== "")
     )
@@ -55,69 +42,95 @@ export const MainPage = () => {
     .sort((a, b) => a.label.localeCompare(b.label));
 
   const handleSearch = () => {
-    const filtered = data.filter((home) => {
+    const filtered = homeData.filter((home) => {
       const price = parseFloat(home?.price?.replace(/[^0-9.-]+/g, ""));
       return (
-        (titleSearchTerm === "" ||
-          home?.beds?.toLowerCase().includes(titleSearchTerm.toLowerCase())) &&
+        (bedsSearchTerm === "" ||
+          home?.beds?.toLowerCase().includes(bedsSearchTerm.toLowerCase())) &&
+        (bathsSearchTerm === "" ||
+          home?.baths?.toLowerCase().includes(bathsSearchTerm.toLowerCase())) &&
         (citySearchTerm === "" ||
           home?.city?.toLowerCase().includes(citySearchTerm.toLowerCase())) &&
-        (price >= priceRange[0] && price <= priceRange[1])
+        price >= priceRange[0] &&
+        price <= priceRange[1]
       );
     });
     setFilteredData(filtered);
     setDisplayCount(60);
+
+    if (citySearchTerm) {
+      const cityData = stateData.find(
+        (data) => data.city.toLowerCase() === citySearchTerm.toLowerCase()
+      );
+      setSelectedCityData(cityData);
+    } else {
+      setSelectedCityData(null);
+    }
   };
 
   const handleClearSearch = () => {
-    setTitleSearchTerm("");
+    setBedsSearchTerm("");
+    setBathsSearchTerm("");
     setCitySearchTerm("");
-    setFilteredData(data);
+    setFilteredData(homeData);
     setDateRange(null);
     setOptions([]); // Reset options for AutoComplete
+    setBOptions([]);
     setDisplayCount(60);
     setPriceRange([0, 100000000]);
+    setSelectedCityData(null);
   };
 
-  const allOptions = useMemo(() => {
-    return Array.from(new Set(data.map((home) => home?.beds)))
-      .map((beds) => ({ value: beds, label: beds }));
-  }, [data]);
+  const bedsOptions = useMemo(() => {
+    return Array.from(new Set(homeData.map((home) => home?.beds))).map(
+      (beds) => ({
+        value: beds,
+        label: beds,
+      })
+    );
+  }, [homeData]);
 
-  const onSearch = (searchText) => {
-    setTitleSearchTerm(searchText);
+  const bathsOptions = useMemo(() => {
+    return Array.from(new Set(homeData.map((home) => home?.baths))).map(
+      (baths) => ({
+        value: baths,
+        label: baths,
+      })
+    );
+  }, [homeData]);
+
+  const onBedsSearch = (searchText) => {
+    setBedsSearchTerm(searchText);
     if (searchText === "") {
-      setOptions(allOptions); // Show all options when search text is empty
+      setOptions(bedsOptions); // Show all options when search text is empty
     } else {
-      const filteredOptions = allOptions.filter((option) =>
+      const filteredOptions = bedsOptions.filter((option) =>
         option.value.toLowerCase().includes(searchText.toLowerCase())
       );
       setOptions(filteredOptions);
     }
   };
 
-
-  const truncateVenueName = (venueName) => {
-    if (!venueName) return '';
-
-    const commas = venueName.split(',');
-
-    if (commas.length <= 2) {
-      // If there are 2 or fewer comma-separated parts, return the whole string
-      return venueName;
+  const onBathsSearch = (searchText) => {
+    setBathsSearchTerm(searchText);
+    if (searchText === "") {
+      setBOptions(bathsOptions); // Show all options when search text is empty
     } else {
-      // If there are more than 2 comma-separated parts, return the first two
-      return commas.slice(0, 2).join(',').trim();
+      const filteredOptions = bathsOptions.filter((option) =>
+        option.value.toLowerCase().includes(searchText.toLowerCase())
+      );
+      setBOptions(filteredOptions);
     }
   };
+
   const inputStyle = {
-    fontFamily: 'Exo',
-    height: '40px',
-    fontSize: '16px',
+    fontFamily: "Exo",
+    height: "40px",
+    fontSize: "16px",
   };
 
   return (
-    <div style={{ fontFamily: 'Exo' }}>
+    <div style={{ fontFamily: "Exo" }}>
       {!loading ? (
         <div className="px-6 pt-[100px] mx-auto max-w-[100rem] lg:px-8">
           <div className="flex flex-col items-center w-full space-y-4 p-4 rounded-lg shadow-sm">
@@ -125,23 +138,38 @@ export const MainPage = () => {
               <AutoComplete
                 className="w-full"
                 options={options}
-                onSearch={onSearch}
-                onSelect={(value) => setTitleSearchTerm(value)}
-                onChange={(value) => setTitleSearchTerm(value)}
-                value={titleSearchTerm}
+                onSearch={onBedsSearch}
+                onSelect={(value) => setBedsSearchTerm(value)}
+                onChange={(value) => setBedsSearchTerm(value)}
+                value={bedsSearchTerm}
               >
                 <Input
                   size="large"
-                  placeholder="Search by title"
+                  placeholder="Search by beds"
                   style={inputStyle}
                 />
               </AutoComplete>
-
+              <AutoComplete
+                className="w-full"
+                options={bOptions}
+                onSearch={onBathsSearch}
+                onSelect={(value) => setBathsSearchTerm(value)}
+                onChange={(value) => setBathsSearchTerm(value)}
+                value={bathsSearchTerm}
+              >
+                <Input
+                  size="large"
+                  placeholder="Search by baths"
+                  style={inputStyle}
+                />
+              </AutoComplete>
               <AutoComplete
                 className="w-full"
                 options={cityOptions}
                 filterOption={(inputValue, option) =>
-                  option?.value?.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1
+                  option?.value
+                    ?.toLowerCase()
+                    .indexOf(inputValue.toLowerCase()) !== -1
                 }
                 onSelect={(value) => setCitySearchTerm(value)}
                 onChange={(value) => setCitySearchTerm(value)}
@@ -154,16 +182,20 @@ export const MainPage = () => {
                 />
               </AutoComplete>
 
-              <RangePicker
+              {/* <RangePicker
                 className="w-full"
                 size="large"
                 onChange={(dates) => {
-                  setDateRange(dates ? [dates[0].startOf('day'), dates[1].endOf('day')] : null);
+                  setDateRange(
+                    dates
+                      ? [dates[0].startOf("day"), dates[1].endOf("day")]
+                      : null
+                  );
                 }}
                 value={dateRange}
                 format="YYYY-MM-DD"
                 style={inputStyle}
-              />
+              /> */}
             </div>
             <div className="w-full max-w-4xl">
               <p className="text-white mb-2">Price Range:</p>
@@ -184,7 +216,7 @@ export const MainPage = () => {
                 type="default"
                 size="large"
                 className="bg-purple-800 text-white hover:bg-purple-900 px-8"
-                style={{ fontFamily: 'Exo' }}
+                style={{ fontFamily: "Exo" }}
               >
                 Search
               </Button>
@@ -193,17 +225,39 @@ export const MainPage = () => {
                 type="default"
                 size="large"
                 className="bg-purple-800 text-white hover:bg-purple-900 px-8"
-                style={{ fontFamily: 'Exo' }}
+                style={{ fontFamily: "Exo" }}
               >
                 Clear
               </Button>
             </div>
           </div>
+          {selectedCityData && (
+            <div
+              className="w-full max-w-4xl mt-4 p-4 bg-gray-800 rounded-lg"
+              style={{ fontFamily: "Exo" }}
+            >
+              <h2 className="text-xl font-semibold text-white mb-2">
+                {selectedCityData.city}, {selectedCityData.state}
+              </h2>
+              <p className="text-gray-300 mb-2">
+                {selectedCityData.description}
+              </p>
+              <div className="flex justify-between">
+                <p className="text-white">
+                  Average Price: {selectedCityData.average_price}
+                </p>
+                <p className="text-white">
+                  Price per Sq Ft: {selectedCityData.sqft_price}
+                </p>
+              </div>
+            </div>
+          )}
           <div className="w-full h-px bg-zinc-800 my-4" />
 
           <div
             className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-2 lg:grid-cols-4"
-            style={{ fontFamily: "inherit" }}>
+            style={{ fontFamily: "inherit" }}
+          >
             {filteredData.slice(0, displayCount).map((home, key) => (
               <div key={key} className="grid grid-cols-1 gap-4 cursor-pointer">
                 <Card className="">
@@ -218,21 +272,37 @@ export const MainPage = () => {
                       />
                     </div>
                     <div className="p-2 rounded-b-xl">
-
                       <h1 className="text-2xl font-semibold text-white truncate">
                         {home?.price}
                       </h1>
-                      {home?.address ? (<p className="text-xl text-gray-300 truncate">{home?.address}</p>) : (<div><br></br></div>)}
+                      {home?.address ? (
+                        <p className="text-xl text-gray-300 truncate">
+                          {home?.address}
+                        </p>
+                      ) : (
+                        <div>
+                          <br></br>
+                        </div>
+                      )}
 
                       <div className="flex justify-between items-center">
-                        {home?.beds ? (<span className="text-md bg-purple-800 text-white px-1 rounded-full">
-                          {home?.beds}
-                        </span>) : (<div></div>)}
+                        {home?.beds ? (
+                          <span className="text-md bg-purple-800 text-white px-1 rounded-full">
+                            {home?.beds}
+                          </span>
+                        ) : (
+                          <div></div>
+                        )}
                         <span className="text-md bg-purple-800 text-white px-1 rounded-full">
                           {home?.baths}
                         </span>
-                        {home?.area ? (<span className="text-md bg-purple-800 text-white px-1 rounded-full">{home?.area} sq ft</span>) : (<div></div>)}
-
+                        {home?.area ? (
+                          <span className="text-md bg-purple-800 text-white px-1 rounded-full">
+                            {home?.area} sq ft
+                          </span>
+                        ) : (
+                          <div></div>
+                        )}
                       </div>
                     </div>
                   </Link>
@@ -243,7 +313,7 @@ export const MainPage = () => {
           <div className="w-full flex justify-center mt-8">
             {displayCount < filteredData.length && (
               <p
-                onClick={() => setDisplayCount(prevCount => prevCount + 60)}
+                onClick={() => setDisplayCount((prevCount) => prevCount + 60)}
                 type="primary"
                 size="large"
                 className="text-white hover:text-violet-500 text-xl mb-10 hover:cursor-pointer"
@@ -259,6 +329,5 @@ export const MainPage = () => {
     </div>
   );
 };
-
 
 export default MainPage;
